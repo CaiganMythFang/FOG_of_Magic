0679532989ea6ef093ff07cfb36f9bc7f78bfd31
Caigan <caiganmythfang@gmail.com>
2022-06-19 03:03:51 -0500

    Unlock Books - Can now be Right Clicked to unlock as backup

---
+++ kubejs/server_scripts/unlock_book_electronics.js
@@ -0,0 +1,12 @@
+events.listen('item.right_click', event => {
+    
+  if (event.item.id == 'contenttweaker:lost_electronics') {
+          
+    event.player.playSound('minecraft:block.beacon.deactivate');
+    
+    event.server.schedule(5, event.server, function (callback) {
+            callback.server.runCommandSilent(`execute as ${event.player.name} in ${event.player.world.dimension} run gamestage add @s lost_knowledge_electronics_depot`);
+            event.player.tell(text.green("You have unlocked Lost Knowledge - Electronics Depot"));
+          })
+      }
+});
